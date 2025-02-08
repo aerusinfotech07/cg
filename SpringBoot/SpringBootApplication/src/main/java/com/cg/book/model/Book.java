272aa17b818a0c2cda640763d44816ac6182b990
aerusinfotech <aerusinfotech@gmail.com>
2025-02-08 11:46:51 +0530

    SpringBoot Crud Operation

--- SpringBoot/SpringBootApplication/src/main/java/com/cg/book/model/Book.java
+++ SpringBoot/SpringBootApplication/src/main/java/com/cg/book/model/Book.java
@@ -1,6 +1,14 @@
 package com.cg.book.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
     private String title;
     private String author;
--- SpringBoot/SpringBootApplication/src/main/java/com/cg/book/service/BookServikceImpl.java
+++ SpringBoot/SpringBootApplication/src/main/java/com/cg/book/service/BookServikceImpl.java
@@ -1,18 +1,20 @@
 package com.cg.book.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cg.book.dao.BookDAO;
+import com.cg.book.dao.BookRepository;
 import com.cg.book.model.Book;
 
 @Service
 public class BookServikceImpl implements BookService{
 
 	@Autowired
-	BookDAO bookDAO;
+	BookRepository bookDAO;
 	
 	
 	@Override
@@ -24,25 +26,31 @@ public class BookServikceImpl implements BookService{
 	@Override
 	public Book saveBook(Book book) {
 		// TODO Auto-generated method stub
-		return bookDAO.saveBook(book);
+		return bookDAO.save(book);
 	}
 
 	@Override
 	public Book updateBook(int id,Book book) {
 		
-		return bookDAO.updateBook(id,book);
+		return bookDAO.save(book);
 	}
 
 	@Override
 	public boolean deleteBook(int id) {
 		// TODO Auto-generated method stub
-		return bookDAO.deleteBook(id);
+		Optional<Book> book=bookDAO.findById(id);
+		if(book.isPresent())
+		{
+			bookDAO.delete(book.get());
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public List<Book> findByTitle(String title) {
 		// TODO Auto-generated method stub
-		return bookDAO.findByTitle(title);
+		return bookDAO.findByTitleContains(title);
 	}
 
 	
