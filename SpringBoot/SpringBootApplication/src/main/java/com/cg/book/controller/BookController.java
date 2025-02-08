78760b26e8fd624d5087f4da65dfa18a99b0f4e0
aerusinfotech <aerusinfotech@gmail.com>
2025-02-08 10:40:44 +0530

    SpringBoot Crud Operation

--- SpringBoot/SpringBootApplication/src/main/java/com/cg/book/controller/BookController.java
+++ SpringBoot/SpringBootApplication/src/main/java/com/cg/book/controller/BookController.java
@@ -3,6 +3,7 @@ package com.cg.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,6 +47,11 @@ public class BookController {
 	}
 	
 	
+	@DeleteMapping("/deleteBook/{id}")
+	public Boolean updateBook(@PathVariable Integer id)
+	{		
+		return bookService.deleteBook(id);
+	}
 	
 	
 	
